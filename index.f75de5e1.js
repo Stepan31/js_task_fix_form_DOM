96a1ef2f31d04c465cacee41c8d3d6f59ec08c18
Stepan Bahrynivskyi <bagrunivskiystepan@gmail.com>
2024-09-06 09:13:21 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const inputs = [
+    ...document.querySelectorAll('input:not([type="submit"]):not([type="button"])')
+];
+inputs.forEach((input)=>{
+    if (!input.id || !input.name) return;
+    const label = document.createElement("label");
+    label.className = "field-label";
+    label.htmlFor = input.id;
+    const labelName = input.name.replace(/([A-Z])/g, " $1").replace(/^./, (str)=>str.toUpperCase());
+    label.textContent = labelName;
+    input.placeholder = labelName;
+    input.before(label);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
